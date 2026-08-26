f15d92ea2e8860440dfbb4c2a297d8e980db001b
ANMOL4355 <anmolojha543@gmail.com>
2026-08-26 20:31:46 +0545

    added data to the db

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Movie)
+admin.site.register(CinemaHall)
+#admin.site.register(Reservation)
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display=["name","cinemahall"]
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display=["cinemahall","movie","show_time","price"]  
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display=["customer","show","seat"]        
--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Movie(models.Model):
+    name=models.CharField(max_length=100)
+    genre=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Seat(models.Model):
+    name=models.CharField(max_length=10)
+    cinemahall=models.ForeignKey(CinemaHall,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Show(models.Model):
+    movie=models.ForeignKey(Movie,on_delete=models.PROTECT)   
+    cinemahall=models.ForeignKey(CinemaHall,on_delete=models.PROTECT)
+    show_time=models.DateTimeField() 
+    price=models.PositiveBigIntegerField()
+
+    class Meta:
+            unique_together=("cinemahall","show_time","movie")
+
+    def __str__(self):
+        return f"{self.movie} - {self.cinemahall}"
+
+
+class Reservation(models.Model):
+    customer=models.ForeignKey(User, on_delete=models.PROTECT)
+    show=models.ForeignKey(Show,on_delete=models.PROTECT)
+    seat=models.ForeignKey(Seat,on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together=("show","seat")
+
+    def __str__(self):
+        return self.customer.first_name
