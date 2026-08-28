02e8021b7fb4f001aba68ed10de02388894c4b38
ANMOL4355 <anmolojha543@gmail.com>
2026-08-28 14:19:19 +0545

    template created and rendered for movie

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+def home(request):
+   
+   latest_movies=Movie.objects.order_by("-release_date")[:8]
+   #print(latest_movies)
+   context={
+        'movies':latest_movies
+   }
+
+   return render(request,"core/home.html",context)
