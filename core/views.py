f753468f47a40a6455a35e3808a806a1c0784909
ANMOL4355 <anmolojha543@gmail.com>
2026-08-31 22:58:25 +0545

    form creation and ,register and login implemented

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Movie,Show
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
 def home(request):
    
@@ -22,4 +25,53 @@ def movie_detail(request,pk):
       'shows':shows
    }
    return render(request,"core/movie_detail.html",context)
- 
+
+
+def register_view(request):
+   if request.method=="POST":
+      #print("Request data:", request.POST)
+      # username=request.POST.get('username')
+      # first_name=request.POST.get('first_name')
+      # last_name=request.POST.get('last_name')
+      # email=request.POST.get('email')
+      # password=request.POST.get('password')
+      form = RegisterForm(data=request.POST)
+      
+      if form.is_valid():
+         #print(form.cleaned_data)
+         data=form.cleaned_data
+         User.objects.create_user(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=data.get('password'),
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name')
+         )
+         print("user creation successful")
+         return redirect('login')
+
+      else:
+         print("form is invalid")
+         return render(request,"core/register.html",{'form':form})
+
+      #print(username,first_name,last_name,email,password)
+      # if len(username)<6:
+      #    print("Username must be at least 6 characters long")
+
+   return render(request,"core/register.html")
+
+
+def login_view(request):
+
+   if request.method == 'POST':
+      #print(request.POST)
+      username=request.POST.get('username')
+      password=request.POST.get('password')
+      #print(username,password)
+      user=authenticate(request,username=username,password=password)
+      if user is not None:
+         login(request,user)
+         print("login successful")
+         return redirect("back")
+
+   return render (request, "core/login.html")
